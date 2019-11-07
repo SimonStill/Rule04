9d8ab3735aadf32da11991eba172ea1b6c081a6b
SimonStill <57300255+SimonStill@users.noreply.github.com>
2019-11-07 18:50:21 -0500

    Add files via upload

---
+++ R04_STR03.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+ *  Rule04 - STR03
+ *
+ ******************************************************************************/
+
+BigInteger x = new BigInteger ("530500452766"); 
+byte [] byteArray = x.toByteArray ( ) ;
+String s = new String(byteArray);
+byteArray = s.getBytes();
+x = new BigInteger(byteArray);
+    
